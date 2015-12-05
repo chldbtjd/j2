68b73209314cab0c5bf5ccc14342339bb761a853
chldbtjd <chldbtjd2272@naver.com>
2015-12-05 19:47:08 +0900

    Videoplayer Homework remake

--- src/com/j2/state/videoplayer/VedioPlayerTestDrive.java
+++ src/com/j2/state/videoplayer/VedioPlayerTestDrive.java
@@ -1,7 +1,7 @@
 package com.j2.state.videoplayer;  
 public class VedioPlayerTestDrive {  
   public static void main(String[] args) {  
-   VideoPlayer video = new VideoPlayer("[Video]");  
+   VideoPlayer video = new VideoPlayer("[Ring]");  
    
    System.out.println("-----------------");  
     System.out.println(video);
--- src/com/j2/state/videoplayer/VideoPlayer.java
+++ src/com/j2/state/videoplayer/VideoPlayer.java
@@ -1,63 +1,76 @@
 package com.j2.state.videoplayer;  
- public class VideoPlayer {  
-    
+
+
+public class VideoPlayer {  
+   
 State onVideoState;  
- State offVideoState;  
+State offVideoState;  
 State playVideoState;  
- State  endVideoState;
-  State state;  
-    String title = null;
-  public VideoPlayer(String title) {  
- onVideoState = new OnvideoState(this);  
-  offVideoState = new OffvideoState(this);   
-  playVideoState = new PlayvideoState(this);  
-   endVideoState = new EndvideoState(this); 
-  this.title = title;  
-    if (title != null) {  
-    state = offVideoState;  
-  } else {  
-    state = endVideoState;  
-   }  
+State  endVideoState;
+ State state;  
+   String title = null;
+ public VideoPlayer(String title) {  
+onVideoState = new OnvideoState(this);  
+ offVideoState = new OffvideoState(this);   
+ playVideoState = new PlayvideoState(this);  
+  endVideoState = new EndvideoState(this); 
+ this.title = title;  
+   if (title != null) {  
+   state = offVideoState;  
+ } else {  
+   state = endVideoState;  
+  }  
+}  
+
+ String getTitle() {  
+    return title;  
+  }  
+
+   
+ public void playVideoState() {  
+  state.playVideoState();  
+ }  
+   
+ public void onVideoState() {  
+  state.onVideoState();  
+ }  
+   
+ public void offVideoState() {  
+  state.offVideoState();  
+}  
+ public void endVideoState(){
+ state.endVideoState();
+ }
+  
+void setState(State state) {  
+  this.state = state;  
+ }  
+      
+public State getState() {  
+   return state;  
+ }  
+ 
+ public State getonVideoState() {  
+   return onVideoState;  
  }  
 
-  
-    
-  public void playVideoState() {  
-   state.playVideoState();  
-  }  
-    
-  public void onVideoState() {  
-   state.onVideoState();  
-  }  
-    
-  public void offVideoState() {  
-   state.offVideoState();  
+ public State getoffVideoState() {  
+   return offVideoState;  
  }  
-  public void endVideoState(){
-  state.endVideoState();
-  }
-   
- void setState(State state) {  
-   this.state = state;  
-  }  
-       
- public State getState() {  
-    return state;  
-  }  
-  
-  public State getonVideoState() {  
-    return onVideoState;  
-  }  
  
-  public State getoffVideoState() {  
-    return offVideoState;  
-  }  
-  
-    public State getplayVideoState() {  
-         return playVideoState;  
-     }  
-  
-     public State getendVideoState() {  
-         return endVideoState;  
-     }  
- }
+   public State getplayVideoState() {  
+        return playVideoState;  
+    }  
+ 
+    public State getendVideoState() {  
+        return endVideoState;  
+    }  
+    public String toString() {  
+       StringBuffer result = new StringBuffer();  
+        result.append("title: " + title);  
+       result.append("\n");  
+       return result.toString();  
+      }  
+     } 
+
+
